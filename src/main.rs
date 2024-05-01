7983581266d55743a9bc222cdf7a64d5db758b14
Wim Looman <git@nemo157.com>
2024-05-01 13:03:07 +0200

    Split packet handling to module

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,26 @@
 use eyre::{OptionExt, Result};
 use tracing::{info, info_span, trace, trace_span};
 use tracing_subscriber::{filter::LevelFilter, layer::SubscriberExt, EnvFilter};
-use zerocopy::{AsBytes, FromBytes, FromZeroes, BE, U16};
+use zerocopy::FromBytes;
+
+mod packet;
+
+use packet::{Command, Init, KeepAlive, Packet, Status, FIDO_CTAPHID_MAX_RECORD_SIZE};
 
 // https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html#usb-discovery
 const FIDO_USAGE_PAGE: u16 = 0xf1d0;
 const FIDO_USAGE_CTAPHID: u16 = 0x01;
-
-// https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html#usb-descriptors
-const FIDO_CTAPHID_MAX_RECORD_SIZE: usize = 64;
-
-#[derive(FromZeroes, FromBytes, PartialEq, Eq, Copy, Clone)]
-#[repr(transparent)]
-struct Command(u8);
-
-impl Command {
-    const KEEPALIVE: Self = Self(0x80 | 0x3b);
-    const MSG: Self = Self(0x80 | 0x03);
-    const CBOR: Self = Self(0x80 | 0x10);
-    const INIT: Self = Self(0x80 | 0x06);
-    const PING: Self = Self(0x80 | 0x01);
-    const CANCEL: Self = Self(0x80 | 0x11);
-    const ERROR: Self = Self(0x80 | 0x3f);
-    const WINK: Self = Self(0x80 | 0x08);
-    const LOCK: Self = Self(0x80 | 0x04);
-}
-
-impl std::fmt::Debug for Command {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = match *self {
-            Self(0..=0x7f) => return write!(f, "invalid continuation treated as command"),
-            Self::KEEPALIVE => "Command::KeepAlive",
-            Self::MSG => "Command::Msg",
-            Self::CBOR => "Command::Cbor",
-            Self::INIT => "Command::Init",
-            Self::PING => "Command::Ping",
-            Self::CANCEL => "Command::Cancel",
-            Self::ERROR => "Command::Error",
-            Self::WINK => "Command::Wink",
-            Self::LOCK => "Command::Lock",
-            _ => "Command::Unknown",
-        };
-
-        f.debug_tuple(name)
-            .field(&format!("{:#x}", self.0 - 0x80))
-            .finish()
-    }
-}
-
-#[derive(FromZeroes, FromBytes)]
-#[repr(C)]
-struct InitPacket {
-    channel: [u8; 4],
-    command: Command,
-    length: U16<BE>,
-    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
-    payload: [u8; 57],
-}
-
-impl std::fmt::Debug for InitPacket {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("InitPacket")
-            .field("channel", &hex::encode(self.channel))
-            .field("command", &self.command)
-            .field("length", &self.length.get())
-            .field("payload", &hex::encode(self.payload))
-            .finish()
-    }
-}
-
-#[derive(FromZeroes, FromBytes)]
-#[repr(C)]
-struct ContinuationPacket {
-    channel: [u8; 4],
-    sequence: u8,
-    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
-    payload: [u8; 59],
-}
-
-impl std::fmt::Debug for ContinuationPacket {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ContinuationPacket")
-            .field("channel", &hex::encode(self.channel))
-            .field("sequence", &self.sequence)
-            .field("payload", &hex::encode(self.payload))
-            .finish()
-    }
-}
-
-#[derive(Debug)]
-enum Packet<'a> {
-    Init(&'a InitPacket),
-    #[allow(dead_code)]
-    Continuation(&'a ContinuationPacket),
-}
-
-#[derive(FromZeroes, FromBytes, AsBytes)]
-#[repr(C)]
-struct RawPacket {
-    channel: [u8; 4],
-    sequence_or_command: u8,
-    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
-    payload: [u8; 59],
-}
-
-impl RawPacket {
-    fn classify(&self) -> Packet<'_> {
-        if self.sequence_or_command < 0x80 {
-            Packet::Continuation(ContinuationPacket::ref_from(self.as_bytes()).unwrap())
-        } else {
-            Packet::Init(InitPacket::ref_from(self.as_bytes()).unwrap())
-        }
-    }
-}
-
-impl std::fmt::Debug for RawPacket {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("RawPacket").field(&self.classify()).finish()
-    }
-}
-
-#[derive(FromZeroes, FromBytes, PartialEq, Eq, Copy, Clone)]
-#[repr(transparent)]
-struct Status(u8);
-
-impl Status {
-    // The authenticator is still processing the current request
-    const PROCESSING: Self = Self(1);
-    // The authenticator is waiting for user presence
-    const UPNEEDED: Self = Self(2);
-}
-
-impl std::fmt::Debug for Status {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = match *self {
-            Self::PROCESSING => "Status::Processing",
-            Self::UPNEEDED => "Status::UserPresenceNeeded",
-            _ => "Status::Unknown",
-        };
-        f.debug_tuple(name).field(&self.0).finish()
-    }
-}
-
-#[derive(FromZeroes, FromBytes, Debug)]
-#[repr(C)]
-struct KeepAlive {
-    status: Status,
-}
 
 #[culpa::try_fn]
 fn process_device(device: hidapi::HidDevice) -> Result<()> {
     let mut buffer = [0; FIDO_CTAPHID_MAX_RECORD_SIZE];
 
     loop {
-        let _len = device.read(&mut buffer)?;
-        let packet = RawPacket::ref_from(&buffer[..]).ok_or_eyre("invalid packet")?;
-
+        let packet = Packet::read_from(&device, &mut buffer)?;
         let _guard = trace_span!("packet", ?packet).entered();
 
-        match packet.classify() {
-            Packet::Init(InitPacket {
+        match packet {
+            Packet::Init(Init {
                 command: Command::KEEPALIVE,
                 payload,
                 ..
---
+++ src/packet.rs
@@ -0,0 +1,158 @@
+use eyre::{OptionExt, Result};
+use zerocopy::{AsBytes, FromBytes, FromZeroes, BE, U16};
+
+// https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html#usb-descriptors
+pub(crate) const FIDO_CTAPHID_MAX_RECORD_SIZE: usize = 64;
+
+#[derive(FromZeroes, FromBytes, PartialEq, Eq, Copy, Clone)]
+#[repr(transparent)]
+pub(crate) struct Command(u8);
+
+impl Command {
+    pub(crate) const KEEPALIVE: Self = Self(0x80 | 0x3b);
+    pub(crate) const MSG: Self = Self(0x80 | 0x03);
+    pub(crate) const CBOR: Self = Self(0x80 | 0x10);
+    pub(crate) const INIT: Self = Self(0x80 | 0x06);
+    pub(crate) const PING: Self = Self(0x80 | 0x01);
+    pub(crate) const CANCEL: Self = Self(0x80 | 0x11);
+    pub(crate) const ERROR: Self = Self(0x80 | 0x3f);
+    pub(crate) const WINK: Self = Self(0x80 | 0x08);
+    pub(crate) const LOCK: Self = Self(0x80 | 0x04);
+}
+
+impl std::fmt::Debug for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match *self {
+            Self(0..=0x7f) => return write!(f, "invalid continuation treated as command"),
+            Self::KEEPALIVE => "Command::KeepAlive",
+            Self::MSG => "Command::Msg",
+            Self::CBOR => "Command::Cbor",
+            Self::INIT => "Command::Init",
+            Self::PING => "Command::Ping",
+            Self::CANCEL => "Command::Cancel",
+            Self::ERROR => "Command::Error",
+            Self::WINK => "Command::Wink",
+            Self::LOCK => "Command::Lock",
+            _ => "Command::Unknown",
+        };
+
+        f.debug_tuple(name)
+            .field(&format!("{:#x}", self.0 - 0x80))
+            .finish()
+    }
+}
+
+#[derive(FromZeroes, FromBytes)]
+#[repr(C)]
+pub(crate) struct Init {
+    pub(crate) channel: [u8; 4],
+    pub(crate) command: Command,
+    pub(crate) length: U16<BE>,
+    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
+    pub(crate) payload: [u8; 57],
+}
+
+impl std::fmt::Debug for Init {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Init")
+            .field("channel", &hex::encode(self.channel))
+            .field("command", &self.command)
+            .field("length", &self.length.get())
+            .field("payload", &hex::encode(self.payload))
+            .finish()
+    }
+}
+
+#[derive(FromZeroes, FromBytes)]
+#[repr(C)]
+pub(crate) struct Continuation {
+    pub(crate) channel: [u8; 4],
+    pub(crate) sequence: u8,
+    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
+    pub(crate) payload: [u8; 59],
+}
+
+impl std::fmt::Debug for Continuation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Continuation")
+            .field("channel", &hex::encode(self.channel))
+            .field("sequence", &self.sequence)
+            .field("payload", &hex::encode(self.payload))
+            .finish()
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum Packet<'a> {
+    Init(&'a Init),
+    #[allow(dead_code)]
+    Continuation(&'a Continuation),
+}
+
+impl Packet<'_> {
+    #[culpa::try_fn]
+    pub(crate) fn read_from<'b>(
+        device: &hidapi::HidDevice,
+        buffer: &'b mut [u8; FIDO_CTAPHID_MAX_RECORD_SIZE],
+    ) -> Result<Packet<'b>> {
+        let len = device.read(buffer)?;
+        // TODO: support buffers shorter than the max
+        assert_eq!(len, buffer.len());
+        RawPacket::ref_from(&buffer[..])
+            .ok_or_eyre("invalid packet")?
+            .classify()
+    }
+}
+
+#[derive(FromZeroes, FromBytes, AsBytes)]
+#[repr(C)]
+struct RawPacket {
+    channel: [u8; 4],
+    sequence_or_command: u8,
+    // TODO: should be [u8], but zerocopy doesn't seem to have helpers for unsized types
+    payload: [u8; 59],
+}
+
+impl RawPacket {
+    fn classify(&self) -> Packet<'_> {
+        if self.sequence_or_command < 0x80 {
+            Packet::Continuation(Continuation::ref_from(self.as_bytes()).unwrap())
+        } else {
+            Packet::Init(Init::ref_from(self.as_bytes()).unwrap())
+        }
+    }
+}
+
+impl std::fmt::Debug for RawPacket {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("RawPacket").field(&self.classify()).finish()
+    }
+}
+
+#[derive(FromZeroes, FromBytes, PartialEq, Eq, Copy, Clone)]
+#[repr(transparent)]
+pub(crate) struct Status(u8);
+
+impl Status {
+    // The authenticator is still processing the current request
+    pub(crate) const PROCESSING: Self = Self(1);
+    // The authenticator is waiting for user presence
+    pub(crate) const UPNEEDED: Self = Self(2);
+}
+
+impl std::fmt::Debug for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match *self {
+            Self::PROCESSING => "Status::Processing",
+            Self::UPNEEDED => "Status::UserPresenceNeeded",
+            _ => "Status::Unknown",
+        };
+        f.debug_tuple(name).field(&self.0).finish()
+    }
+}
+
+#[derive(FromZeroes, FromBytes, Debug)]
+#[repr(C)]
+pub(crate) struct KeepAlive {
+    pub(crate) status: Status,
+}
