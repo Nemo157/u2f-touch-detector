44a1cea93cb9e09ea14ea52ddedf15fd47fb721d
Wim Looman <git@nemo157.com>
2024-07-19 11:40:02 +0200

    Allow setting per-device notification messages

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 use confique::Config as _;
 use directories::ProjectDirs;
 use eyre::{OptionExt, Result};
+use serde::de::{Deserialize, DeserializeOwned, Deserializer};
+use std::{collections::HashMap, hash::Hash};
 
 #[derive(confique::Config, Debug)]
 #[config(partial_attr(derive(Clone, Debug)))]
@@ -27,5 +29,96 @@ impl Config {
             .env()
             .file(dirs.config_dir().join("config.toml"))
             .load()?
+    }
+}
+
+#[derive(Debug)]
+pub struct ConfigMap<K: DeserializeOwned + Eq + Hash, V: confique::Config> {
+    pub inner: HashMap<K, V>,
+}
+
+#[derive(Debug)]
+pub struct ConfigMapPartial<K: DeserializeOwned + Eq + Hash, V: confique::Partial> {
+    pub inner: HashMap<K, V>,
+}
+
+impl<K: DeserializeOwned + Eq + Hash, V: confique::Config> confique::Config for ConfigMap<K, V> {
+    type Partial = ConfigMapPartial<K, V::Partial>;
+
+    const META: confique::meta::Meta = confique::meta::Meta {
+        name: "",
+        doc: &[],
+        fields: &[],
+    };
+
+    fn from_partial(partial: Self::Partial) -> Result<Self, confique::Error> {
+        // TODO: this needs to use `confique::internal::map_err_prefix_path` to give the correct path in errors
+        let inner: Result<_, confique::Error> = partial
+            .inner
+            .into_iter()
+            .map(|(k, v)| Ok((k, V::from_partial(v)?)))
+            .collect();
+        Ok(Self { inner: inner? })
+    }
+}
+
+impl<'de, K: DeserializeOwned + Eq + Hash, V: confique::Partial> Deserialize<'de>
+    for ConfigMapPartial<K, V>
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(Self {
+            inner: HashMap::deserialize(deserializer)?,
+        })
+    }
+}
+
+impl<K: DeserializeOwned + Eq + Hash, V: confique::Partial> confique::Partial
+    for ConfigMapPartial<K, V>
+{
+    fn empty() -> Self {
+        Self {
+            inner: HashMap::new(),
+        }
+    }
+
+    fn default_values() -> Self {
+        Self::empty()
+    }
+
+    fn from_env() -> Result<Self, confique::Error> {
+        // TODO: dunno if this makes sense to support somehow
+        Ok(Self::empty())
+    }
+
+    fn with_fallback(mut self, fallback: Self) -> Self {
+        for (k, v) in fallback.inner {
+            let v = match self.inner.remove(&k) {
+                Some(value) => value.with_fallback(v),
+                None => v,
+            };
+            self.inner.insert(k, v);
+        }
+        self
+    }
+
+    fn is_empty(&self) -> bool {
+        self.inner.is_empty()
+    }
+
+    fn is_complete(&self) -> bool {
+        self.inner.values().all(|v| v.is_complete())
+    }
+}
+
+impl<K: DeserializeOwned + Eq + Hash + Clone, V: confique::Partial + Clone> Clone
+    for ConfigMapPartial<K, V>
+{
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
     }
 }
--- src/notify.rs
+++ src/notify.rs
@@ -1,3 +1,4 @@
+use crate::config::ConfigMap;
 use eyre::Result;
 use notify_rust::{Notification, Timeout, Urgency};
 use std::{
@@ -8,16 +9,39 @@ use tracing::warn;
 
 #[derive(confique::Config, Debug)]
 #[config(partial_attr(derive(Clone, Debug)))]
-#[config(partial_attr(serde(deny_unknown_fields)))]
+#[config(partial_attr(serde(deny_unknown_fields, rename_all = "kebab-case")))]
+pub struct DeviceConfig {
+    /// Override notification heading for this device
+    heading: Option<String>,
+
+    /// Override notification message for this device
+    message: Option<String>,
+}
+
+#[derive(confique::Config, Debug)]
+#[config(partial_attr(derive(Clone, Debug)))]
+#[config(partial_attr(serde(deny_unknown_fields, rename_all = "kebab-case")))]
 pub struct Config {
     /// Enable module
     #[config(default = false)]
     pub enable: bool,
+
+    /// Notification heading
+    #[config(default = "U2F Touch Required")]
+    heading: String,
+
+    // TODO: Maybe make this use a template string so it's possible to do something like the default
+    /// Notification message, default is "Device {serial}"
+    message: Option<String>,
+
+    /// Override config for a specific device, indexed by device serial number
+    #[config(nested)]
+    devices: ConfigMap<String, DeviceConfig>,
 }
 
 #[culpa::try_fn]
 pub(crate) fn run(
-    _config: Config,
+    config: Config,
     mut rx: tokio::sync::broadcast::Receiver<(Arc<str>, bool)>,
 ) -> Result<()> {
     let mut active = HashMap::new();
@@ -25,12 +49,32 @@ pub(crate) fn run(
     while let Ok((serial, needed)) = rx.blocking_recv() {
         match (needed, active.entry(serial.clone())) {
             (true, Entry::Vacant(entry)) => {
+                let device = config.devices.inner.get(&*serial);
+
+                let summary = device
+                    .and_then(|d| d.heading.as_deref())
+                    .unwrap_or(&config.heading);
+
+                let body_tmp;
+                let body = match device
+                    .and_then(|d| d.message.as_deref())
+                    .or(config.message.as_deref())
+                {
+                    Some(message) => message,
+                    None => {
+                        // TODO(rustc 1.79): no tmp needed
+                        body_tmp = format!("Device {serial}");
+                        &body_tmp
+                    }
+                };
+
                 let notification = Notification::new()
                     .timeout(Timeout::Never)
                     .urgency(Urgency::Critical)
-                    .summary("U2F Touch Required")
-                    .body(&format!("Device {serial}"))
+                    .summary(summary)
+                    .body(body)
                     .finalize();
+
                 match notification.show() {
                     Ok(handle) => {
                         entry.insert(handle);
